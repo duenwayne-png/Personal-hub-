19d5a58d41435fd31edf89116d4879bd62e2fa15
duenwayne-png <duenwayne@gmail.com>
2026-06-30 10:10:46 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "personal-hub-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
+
+self.addEventListener("push", (e) => {
+  const data = e.data?.json() || { title: "Personal Hub", body: "You have a new update!" };
+  e.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: "/icon-192.png",
+      vibrate: [200, 100, 200],
+    })
+  );
+});
